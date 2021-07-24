84029e23679d5f4828a25719448964e29d9582af
7hr0wer <throwerbai@163.com>
2021-07-24 15:52:54 +0800

    误删除之后的重新提交。

---
+++ ProxyValidator/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace ProxyValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(171, 238);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(189, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始验证";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "proxy.txt";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "导入代理";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "验证网址：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 21);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "https://www.baidu.com/";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 43);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "测试网址";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(189, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 43);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "导出结果";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 274);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ProxyValidator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ProxyValidator/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace ProxyValidator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] proxy;
+        int a;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(new ThreadStart(Thread1));
+            thread1.Start();
+        }
+
+        private void Thread1()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button4.Enabled = false;
+            textBox1.Enabled = false;
+            richTextBox1.Clear();
+            label1.Text = "正在验证代理......";
+            for (int i=0;i<proxy.Length;i++)
+            {
+                a = i;
+                Thread thread2 = new Thread(new ThreadStart(Thread2));
+                thread2.Start();
+                System.Threading.Thread.Sleep(100);
+            }
+            if(richTextBox1.Text=="")
+            {
+                label1.Text = "验证完毕！无可用代理！";
+                MessageBox.Show("验证完毕！无可用代理！", "提示");
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button4.Enabled = true;
+                textBox1.Enabled = true;
+            }
+            else
+            {
+                label1.Text = "验证完毕！";
+                MessageBox.Show("验证完毕！", "提示");
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button4.Enabled = true;
+                textBox1.Enabled = true;
+            }
+        }
+        private void Thread2()
+        {
+            try
+            {
+                WebProxy proxyObject = new WebProxy(proxy[a]);
+                HttpWebRequest Req = (HttpWebRequest)WebRequest.Create(textBox1.Text);
+                Req.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; QQWubi 133; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; CIBA; InfoPath.2)";//UA
+                Req.Proxy = proxyObject;
+                Req.Method = "GET";
+                Req.Timeout = 1000;
+                HttpWebResponse Resp = (HttpWebResponse)Req.GetResponse();
+                richTextBox1.Text += proxy[a] + " 可用\n";
+            }
+            catch
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread4 = new System.Threading.Thread(new System.Threading.ThreadStart(Thread4));
+            thread4.ApartmentState = ApartmentState.STA;
+            thread4.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread thread3 = new Thread(new ThreadStart(Thread3));
+            thread3.Start();
+        }
+        private void Thread3()
+        {
+            button2.Enabled = false;
+            button4.Enabled = false;
+            textBox1.Enabled = false;
+            try
+            {
+                Encoding encoding = Encoding.UTF8;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(textBox1.Text);
+                request.Method = "GET";
+                request.Accept = "text/html, application/xhtml+xml, */*";
+                request.ContentType = "application/json";
+                request.Timeout = 1000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                MessageBox.Show("验证网址可用！", "提示");
+                button2.Enabled = true;
+                textBox1.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("验证网址不可用！", "提示");
+                button2.Enabled = true;
+                button4.Enabled = true;
+                textBox1.Enabled = true;
+            }
+        }
+        private void Thread4()
+        {
+            label1.Text = "正在导入代理......";
+            openFileDialog1.Filter = "txt文件|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                proxy = File.ReadAllLines(openFileDialog1.FileName);
+                int i = 0;
+                richTextBox1.Clear();
+                while (i<proxy.Length)
+                {
+                    richTextBox1.Text += proxy[i] + "\r";
+                    i++;
+                }
+                button1.Enabled = true;
+                label1.Text = "代理已导入！";
+            }
+            else
+            {
+                label1.Text = "代理已导入！";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Thread thread5 = new System.Threading.Thread(new System.Threading.ThreadStart(Thread5));
+            thread5.ApartmentState = ApartmentState.STA;
+            thread5.Start();
+        }
+        private void Thread5()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "(*.txt)|*.txt|(*.*)|*.*";
+            saveFileDialog.FileName = "result.txt";
+            bool flag = saveFileDialog.ShowDialog() == DialogResult.OK;
+            if (flag)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName, true);
+                streamWriter.Write(this.richTextBox1.Text);
+                streamWriter.Close();
+            }
+        }
+    }
+}
